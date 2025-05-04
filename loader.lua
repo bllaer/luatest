5eb3b5d57c4236e8944a766c2b7a866cf1285f7a
bllaer <34323413+bllaer@users.noreply.github.com>
2025-05-04 14:06:50 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -3,7 +3,7 @@ local function getScript(url)
     return loadstring(game:HttpGet(url))()
 end
 
-local ui = getScript("https://raw.githubusercontent.com/yourusername/yourrepo/main/ui.lua")
-local onClick = getScript("https://raw.githubusercontent.com/yourusername/yourrepo/main/test_button.lua")
+local ui = getScript("https://raw.githubusercontent.com/bllaer/luatest/main/ui.lua")
+local onClick = getScript("https://raw.githubusercontent.com/bllaer/luatest/main/test_button.lua")
 
 ui.MouseButton1Click:Connect(onClick)
